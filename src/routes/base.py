b8f4e86556afc7e2f12766a92cea2bdea12d8c09
kerolos <kerolos20140659@yahoo.com>
2024-10-28 21:51:31 +0200

    update

--- src/routes/base.py
+++ src/routes/base.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI,APIRouter
 import os
-from helpers.config import Settings
+from src.helpers.config import Settings
 
 base_router=APIRouter()
 
@@ -9,7 +9,7 @@ def welcome():
     app_settings=Settings()
     app_name=app_settings.APP_NAME
     app_version=app_settings.APP_VERSION
-    
+
 
 
     return {
